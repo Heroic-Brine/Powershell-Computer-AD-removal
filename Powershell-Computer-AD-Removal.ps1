41ce0ba3c048524b080fc18c76aff95b62c7ad21
Heroic-Brine <stefanotoft101112@live.dk>
2020-03-27 09:14:30 +0100

    added restart check

--- Powershell-Computer-AD-Removal.ps1
+++ Powershell-Computer-AD-Removal.ps1
@@ -8,6 +8,22 @@ param(
 #password is collected here directly as secure strig
 Remove-Computer -credential $username -force
 Start-Sleep 2
-Write-Host "Goodbye"
+Write-Host "you will need to restart for this to take effect do you want to do this now?"
+write-host  "( y / N ) " -ForegroundColor Yellow -NoNewline
+$YesNo = Read-Host
+switch ($YesNo)
+{
+     Y{
+          'restarting computer Goodbye!'
+          Restart-Computer -delay 2
+     }
+     N{
+          'Ok Goodbye!'
+     }
+     
+     Default {
+          'ok Goodbye!'
+     }
+}
 Start-Sleep 3
 exit
